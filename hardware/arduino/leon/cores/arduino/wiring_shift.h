9863d577c75d709c39c5f909028f1f6afe789e7d
Yen-Kuo Hsu <yen-kuo.hsu@skytraq.com.tw>
2015-02-12 16:26:00 +0800

    Removed PCD8544 from wiring_shift

--- hardware/arduino/leon/cores/arduino/wiring_shift.h
+++ hardware/arduino/leon/cores/arduino/wiring_shift.h
@@ -26,23 +26,6 @@
 
 #include "stdint.h"
 
-#define	PCD8544_INSTRUCTION	0
-#define	PCD8544_NORMAL_DATA	1
-#define	PCD8544_RESET				2
-
-typedef	struct _pcd844_data
-{
-	uint16_t numData;
-	uint8_t *type; // 0: instruction, 1: normal data, 2: reset
-  uint8_t *data;
-} sPCD8544Data, *pPCD8544Data;
-
-typedef	struct _pcd844_digit
-{
-	uint8_t type[5];
-  uint8_t data[5];
-} sPCD8544Digit, *pPCD8544Digit;
-
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -54,21 +37,6 @@ extern "C" {
 // declarations for public functions
 void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint8_t val);
 uint8_t shiftIn(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder);
-
-// defines for pins for PCD8544
-#define	PCD8544_LED_PIN		3
-#define	PCD8544_SCE_PIN		10
-#define	PCD8544_DC_PIN		16	// gpio 22 has bug waiting for fix
-#define	PCD8544_SCLK_PIN	30
-#define	PCD8544_SDIN_PIN	28
-#define	PCD8544_RES_PIN		5
-
-void pcd8544SerialBus(uint8_t sce_n, uint8_t dc_n, uint8_t sclk, uint8_t sdin, uint8_t res_n, pPCD8544Data ptr);
-void pcd8544Init(void);
-void pcd8544CleanScreen(void);
-void pcd8544SetCursor(uint8_t bank, uint8_t x);
-void pcd8544DigitPlot(void);
-void pcd8544FractalPlot(void);
 
 #ifdef __cplusplus
 }
